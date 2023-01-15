3e4b4609e2ff24b7c1cfbb1a4cb3eef986e73d63
ZhukovaYu <zhukova.julia@gmail.com>
2023-01-15 20:21:42 +0100

    goit-js-hw-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve(`Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`Rejected promise ${position} in ${delay}ms`);
+    }
+  });
+};
+
+const onFormSubmit = el => {
+  el.preventDefault();
+  const step = el.currentTarget.elements.step.value;
+  let delay = el.currentTarget.elements.delay.value;
+  const amount = el.currentTarget.elements.amount.value;
+  for (let i = 0; i < amount; i += 1) {
+    setTimeout(() => {
+      createPromise(i, delay)
+        .then(({ position, delay }) => { Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`); })
+        .catch(({ position, delay }) => { Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`); });
+      delay += step;
+    }
+  )};
+};
+
+form.addEventListener('submit', onFormSubmit);
